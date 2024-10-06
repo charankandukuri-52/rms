cbe3371deb9902d677759a61ad85313e15ac0893
Kandukuri Charan <119528567+charankandukuri-52@users.noreply.github.com>
2024-10-06 11:50:30 +0530

    Frontend Changes

--- Frontend/src/app/HomePage/home-page/home-page.component.ts
+++ Frontend/src/app/HomePage/home-page/home-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { HeroSectionComponent } from "../hero-section/hero-section.component";
-
+import { BookTableComponent } from '../../features/book-table/book-table.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, HeroSectionComponent],
+  imports: [NavbarComponent, HeroSectionComponent, BookTableComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
--- Frontend/src/app/app-data/manage-restuarants/manage-restuarants.component.ts
+++ Frontend/src/app/app-data/manage-restuarants/manage-restuarants.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RegistrationComponent } from "../../features/restaurant-registration/registration/registration.component";
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-manage-restuarants',
   standalone: true,
-  imports: [],
+  imports: [RegistrationComponent, CommonModule],
   templateUrl: './manage-restuarants.component.html',
-  styleUrl: './manage-restuarants.component.css'
+  styleUrl: './manage-restuarants.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
 })
 export class ManageRestuarantsComponent {
-
+activeTab:string = 'add'
+setActiveTab(tabName:string) {
+  this.activeTab = tabName
+}
 }
